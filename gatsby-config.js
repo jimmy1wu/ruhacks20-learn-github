3f512adea8b59c88f5c1b7a8314116b7ba150736
Ryan Zegray <ryan.zegray@gmail.com>
2020-05-15 11:18:11 -0400

    add learn git

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
 module.exports = {
   siteMetadata: {
     title: `Learn git`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Learn git`,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `data`,
+        path: `${__dirname}/src/data`,
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-json`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `learn-git`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -27,8 +28,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
